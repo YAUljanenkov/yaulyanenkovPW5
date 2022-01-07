36b189fb696dd9df93ca29cbf1f18c4269e07c08
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2022-01-07 23:43:18 +0300

    Add shimmers

--- yaaulyanenkovPW5/Article/ArticleCell.swift
+++ yaaulyanenkovPW5/Article/ArticleCell.swift
@@ -11,10 +11,17 @@ import UIKit
 
 class ArticleCell: UITableViewCell {
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        contentConfiguration = defaultContentConfiguration()
-    }
+    var loadedImage:  UIImageView = {
+        let shimmerView = ShimmerView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        shimmerView.startAnimating()
+        shimmerView.contentMode = .scaleAspectFit
+        return shimmerView
+    }()
+    
+    var title: String?
+    var announce: String?
+    var url: URL?
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -24,6 +31,42 @@ class ArticleCell: UITableViewCell {
         super.init(coder: coder)
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        loadedImage.startAnimating()
+        contentConfiguration = defaultContentConfiguration()
+    }
+    
+    override func updateConfiguration(using state: UICellConfigurationState) {
+        var content = self.defaultContentConfiguration().updated(for: state)
+        content.text = title
+        content.secondaryText = announce
+        self.contentConfiguration = content
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 100, bottom: 0, right: 0))
+    }
+    
+    func setContent(article: Article.Fetch.ArticleModel) {
+        title = article.title
+        announce = article.announce
+        url = article.articleUrl
+        DispatchQueue.global().async {
+            let image = self.loadImage(url: article.img?.url)
+            DispatchQueue.main.async {
+                self.loadedImage.stopAnimating()
+                self.loadedImage.image = image
+            }
+        }
+        setNeedsUpdateConfiguration()
+        self.addSubview(loadedImage)
+        self.sizeToFit()
+        loadedImage.pinTop(to: self, 16)
+        loadedImage.pinLeft(to: self, 8)
+    }
+    
     private func loadImage(url: URL?) -> UIImage? {
         guard let url = url else {
             return nil
@@ -31,41 +74,37 @@ class ArticleCell: UITableViewCell {
         guard let data = try? Data(contentsOf: url) else {
             return nil
         }
-        return UIImage(data: data)
+        return UIImage(data: data)?.scalePreservingAspectRatio(targetSize: CGSize(width: 100, height: 100))
     }
-    
-    var loadedImage: UIImage? {
-        didSet {
-            DispatchQueue.main.async {
-                self.setNeedsUpdateConfiguration()
-            }
+}
+
+
+extension UIImage {
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+        // Determine the scale factor that preserves aspect ratio
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let scaleFactor = min(widthRatio, heightRatio)
+        
+        // Compute the new image size that preserves aspect ratio
+        let scaledImageSize = CGSize(
+            width: size.width * scaleFactor,
+            height: size.height * scaleFactor
+        )
+
+        // Draw and return the resized UIImage
+        let renderer = UIGraphicsImageRenderer(
+            size: scaledImageSize
+        )
+
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+                origin: .zero,
+                size: scaledImageSize
+            ))
         }
+        
+        return scaledImage
     }
-    
-    var title: String?
-    var announce: String?
-    var url: URL?
-    
-    override func updateConfiguration(using state: UICellConfigurationState) {
-        var content = self.defaultContentConfiguration().updated(for: state)
-        content.text = title
-        content.secondaryText = announce
-        content.image = self.loadedImage
-        content.imageProperties.reservedLayoutSize = CGSize(width: 120, height: 100)
-        content.imageProperties.maximumSize = CGSize(width: 120, height: 100)
-        self.contentConfiguration = content
-    }
-    
-    func setContent(article: Article.Fetch.ArticleModel) {
-        title = article.title
-        announce = article.announce
-        loadedImage = UIImage(named: "kitten")
-        url = article.articleUrl
-        DispatchQueue.global().async {
-            self.loadedImage = self.loadImage(url: article.img?.url)
-        }
-        setNeedsUpdateConfiguration()
-    }
-    
-    
 }
